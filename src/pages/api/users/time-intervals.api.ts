99bde291ec4a1fe1a1dcb23165c23a6e197677cc
Amós Rodrigues <amos.adm.rh@gmail.com>
2023-04-12 22:24:52 -0300

    fix: alter verify method post.

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -18,7 +18,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== 'POST') {
+  if (!req?.method || req.method !== 'POST') {
     return res.status(405).end()
   }
 
